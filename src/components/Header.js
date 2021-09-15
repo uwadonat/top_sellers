0812d191bc1f951f1595f9d9087ab93f49a6f8e8
uwadonat <uwadonat@gmail.com>
2021-09-15 16:38:23 +0300

    Add link to back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,15 @@ import { NavLink } from 'react-router-dom';
 import MicIcon from '@material-ui/icons/Mic';
 import SettingsIcon from '@material-ui/icons/Settings';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import {
+    Link,
+  } from 'react-router-dom';
 import './Header.css';
 function Header() {
     return (
        
         <header>
-            <div><ArrowBackIosIcon /></div>
+            <div><Link to="/" ><ArrowBackIosIcon /></Link></div>
             <div>TOP SELLERS</div>
             <div>
                 <MicIcon className="Heade-icons"/>
